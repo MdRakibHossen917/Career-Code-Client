2cc43021b915ca61c72337e9d95bf252f0b313a8
MdRakibHossen0 <mdrakibhossencse@gmail.com>
2025-05-29 07:53:31 +0600

    SignIn page done

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,66 +1,87 @@
-import React from 'react';
-import { NavLink } from 'react-router';
-
-
-
+import React, { use } from "react";
+import { NavLink } from "react-router";
+import { AuthContext } from "../../contexts/AuthContext/AuthContext";
 
 const Navbar = () => {
-    const links = (
-      <>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/">Find a Job</NavLink>
-        </li>
-        <li>
-          <NavLink to="/">Recruiters</NavLink>
-        </li>
+  const { user, signOutUser } = use(AuthContext);
 
-        <li>
-          <NavLink to="/">Candidates</NavLink>
-        </li>
-      </>
-    );
-    return (
-      <div className="navbar bg-base-100 shadow-sm">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+  const handleSignOut=()=>{
+    signOutUser()
+      .then(( ) => {
+       console.log('user Sign Out');
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }
+  const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Find a Job</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Recruiters</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/">Candidates</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              {links}
-            </ul>
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{links}</ul>
-        </div>
-        <div className="navbar-end">
-          <NavLink className="btn" to='/register'>Register</NavLink>
-          <NavLink className="btn" to='/signIn'>SignIn</NavLink>
-        </div>
+        <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
-    );
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end">
+        {user ? (
+          <button onClick={handleSignOut} className="btn">
+            SignOut
+          </button>
+        ) : (
+          <>
+            <NavLink className="btn" to="/register">
+              Register
+            </NavLink>
+            <NavLink className="btn" to="/signIn">
+              SignIn
+            </NavLink>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React, { use, useState } from "react";
+import signInLottieFile from "../../assets/SignIn.json";
+import Lottie from "lottie-react";
+import { AuthContext } from "../../contexts/AuthContext/AuthContext";
 
 const SignIn = () => {
-    return (
-        <div>
-            
+  const { signInUser } = use(AuthContext);
+  const [errorMessage,setErrorMessage]=useState()
+  const handleSignIn = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    console.log(email, password);
+
+    //SignIn user
+    signInUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+        alert("login successful");
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setErrorMessage(error.message);
+      });
+  };
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          {/*Lottie Animation*/}
+          <Lottie
+            style={{ width: "300px" }}
+            animationData={signInLottieFile}
+            loop={true}
+          ></Lottie>
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <h1 className="text-4xl font-bold">SignIn Now!</h1>
+            <form onSubmit={handleSignIn}>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  className="input"
+                  name="email"
+                  placeholder="Email"
+                />
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  name="password"
+                  placeholder="Password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <p className="text-orange-600">{errorMessage}</p>
+                <button className="btn btn-neutral mt-4">Register</button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
